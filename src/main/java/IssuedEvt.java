58ddb2d1b3497db5090552068abf782f7555b39f
Adam Siemion <adam.siemion+github@gmail.com>
2018-12-27 11:43:13 +0100

    quick start

---
+++ src/main/java/IssuedEvt.java
@@ -0,0 +1,15 @@
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@AllArgsConstructor
+public class IssuedEvt {
+    private final String id;
+    private final Integer amount;
+}
